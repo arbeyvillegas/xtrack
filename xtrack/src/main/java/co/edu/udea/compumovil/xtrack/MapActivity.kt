bca3b83eaefd5bdc703d791105fe30e46eccee40
jharpx10 <juana.rios@udea.edu.co>
2022-03-06 20:06:44 -0500

    se agrega activity de mapa

---
+++ xtrack/src/main/java/co/edu/udea/compumovil/xtrack/MapActivity.kt
@@ -0,0 +1,28 @@
+package co.edu.udea.compumovil.xtrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map:GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment:SupportMapFragment=supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+       map=googleMap
+    }
+}
