f868a767f65f5a8387c932e81d72a0f563e845db
jharpx10 <juana.rios@udea.edu.co>
2022-03-06 21:08:35 -0500

    se ajusta fragment de mapa y se divide de la activity map

--- xtrack/src/main/java/co/edu/udea/compumovil/xtrack/MapActivity.kt
+++ xtrack/src/main/java/co/edu/udea/compumovil/xtrack/MapActivity.kt
@@ -14,7 +14,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-        createFragment()
+     
     }
 
     private fun createFragment() {
