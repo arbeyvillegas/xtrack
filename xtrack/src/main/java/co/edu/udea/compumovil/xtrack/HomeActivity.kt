bfc5f44102073d995c952008f95ce0a7302a73e8
Carlos Herrera <carlos.herrera2@udea.edu.co>
2022-03-08 12:19:54 -0500

    Call Activity Experience

--- xtrack/src/main/java/co/edu/udea/compumovil/xtrack/HomeActivity.kt
+++ xtrack/src/main/java/co/edu/udea/compumovil/xtrack/HomeActivity.kt
@@ -1,10 +1,12 @@
 package co.edu.udea.compumovil.xtrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import co.edu.udea.compumovil.xtrack.adapter.ExperienceAdapter
 import co.edu.udea.compumovil.xtrack.data.DataSource
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +17,21 @@ class HomeActivity : AppCompatActivity() {
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ExperienceAdapter(this, experienceDataset)
+
+
+
+        /*  Pasar a la activity
+            de Experince
+         */
+
+        val botonAddExperience = findViewById<FloatingActionButton>(R.id.botonAddExperience)
+
+        botonAddExperience.setOnClickListener{
+
+            val intentAddExperience = Intent(this, ExperienceActivity::class.java)
+            startActivity(intentAddExperience)
+
+        }
+
     }
 }
