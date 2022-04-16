009f1b924d93968cd4c3a6ec16fa0c3d2072d56b
unknown <juana.rios@udea.edu.co>
2022-04-16 15:50:57 -0500

    se añade el poder tomar y seleccionar fotos

--- xtrack/src/main/java/co/edu/udea/compumovil/xtrack/TakeSelectPhotoActivity.kt
+++ xtrack/src/main/java/co/edu/udea/compumovil/xtrack/TakeSelectPhotoActivity.kt
@@ -1,99 +1,160 @@
 package co.edu.udea.compumovil.xtrack
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.widget.ImageView
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-
+import  co.edu.udea.compumovil.xtrack.databinding.ActivityTakeSelectPhotoBinding
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 
 class TakeSelectPhotoActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityTakeSelectPhotoBinding
+    private lateinit var imageURI: Uri
 
 
-    @RequiresApi(Build.VERSION_CODES.M)
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_take_select_photo)
-        setImageListener()
-
+        binding = ActivityTakeSelectPhotoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        requestPermission()
+        setupListeners()
 
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun setImageListener() {
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun setupListeners() {
+        binding.buttonTakeFromGalery.setOnClickListener { pickPhotoFromGallery() }
+        binding.buttonTakePhoto.setOnClickListener { takePhoto() }
+        binding.buttonSavePhoto.setOnClickListener { saveImage(this@TakeSelectPhotoActivity, imageURI) }
 
-        findViewById<ImageView>(R.id.takeSelectPhotoImageView).setOnClickListener {
-            handleImageClick()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun takePhoto() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePhoto ->
+            takePhoto.resolveActivity(packageManager)?.also {
+                startForResult.launch(takePhoto)
+
+            }
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun handleImageClick() {
-        val pick = true
-        if (pick) {
-            if (!checkCameraPermission()) {
-                requestCameraPermission()
-            } else {
-                pickImage()
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun pickPhotoFromGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startForResult.launch(intent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun requestPermission() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when (PackageManager.PERMISSION_GRANTED) {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) -> {
+                }
+                else -> requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
             }
+            when (PackageManager.PERMISSION_GRANTED) {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) -> {
+                }
+                else -> requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data?.data
+                if (data == null) {
+                    result.data?.extras?.let { bundle ->
+                        val bitmap = bundle.get("data") as Bitmap
+
+                        val bytes = ByteArrayOutputStream()
+                        bitmap!!.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+                        val path: String = MediaStore.Images.Media.insertImage(
+                            contentResolver,
+                            bitmap,
+                            "Title",
+                            null
+                        )
+                        imageURI = Uri.parse(path);
+                    }
+                } else {
+                    imageURI = data;
+
+                }
+                binding.imageViewSelectedPhoto.setImageURI(imageURI)
+            }
+        }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+
+        if (isGranted) {
+            pickPhotoFromGallery()
         } else {
-            if (!checkStoragePermission()) {
-                requestStoragePermission()
-            } else {
-                pickImage()
-            }
-
+            Toast.makeText(
+                this,
+                "Permission denied",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
-    private fun pickImage() {
+
+    private fun saveImage(context: Context, uri: Uri) {
+        val id = generateId()
+        val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+
+        try {
+            val file = File(context.filesDir, id+"image.jpg");
+            val fOut = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut)
+            fOut.flush()
+            fOut.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.i(null, "Save file error!")
+            return
+        }
+
+        var i=0;
+
 
 
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun requestStoragePermission() {
-        requestPermissions(
-            arrayOf(
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ), 100
-        )
+    private fun generateId(): String {
+        return System.currentTimeMillis().toString();
     }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun requestCameraPermission() {
-        requestPermissions(
-            arrayOf(
-                android.Manifest.permission.CAMERA,
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ), 100
-        )
-    }
-
-    private fun checkStoragePermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun checkCameraPermission(): Boolean {
-        val res1 = ContextCompat.checkSelfPermission(
-            this,
-            android.Manifest.permission.CAMERA
-        ) == PackageManager.PERMISSION_GRANTED
-        val res2 = ContextCompat.checkSelfPermission(
-            this,
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
-        return res1 && res2
-    }
-
-
-
 
 }
