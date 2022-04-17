db73362db662f9344b79102e01882e44a105c4b6
unknown <juana.rios@udea.edu.co>
2022-04-16 22:10:10 -0500

    se añade la actividad de carousel

---
+++ xtrack/src/main/java/co/edu/udea/compumovil/xtrack/CarouselActivity.kt
@@ -0,0 +1,13 @@
+package co.edu.udea.compumovil.xtrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CarouselActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carousel)
+
+
+    }
+}
